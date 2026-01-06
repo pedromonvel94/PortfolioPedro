fa2a795de526d8b06c9625359ae260f15c350f07
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2026-01-05 23:52:31 -0500

    setup tailwind build for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.html", "./**/*.html", "./**/*.js"],
   theme: {
     screens: {
       ssm: '250px',
